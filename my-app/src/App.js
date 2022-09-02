8e193dff327958add73ffb172bdc4cdd3a8bfdb3
Olivia Pomeroy <opomeroy26@gmail.com>
2022-09-02 09:46:30 -0700

    Created Home Page

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './Home'; 
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <p>
-          {/**/ }
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-        
-        </a>
-      </header>
+     <Home />
     </div>
   );
 }
