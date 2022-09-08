a6888e873cbbb72ed43c10e12863f72f252227e9
Olivia Pomeroy <opomeroy26@gmail.com>
2022-09-07 20:21:44 -0700

    Idea of layout for homepage

--- my-app/src/Home.js
+++ my-app/src/Home.js
@@ -3,7 +3,7 @@ import React from "react";
 function Home() {
     return (
         <div>
-            Home Page 
+            Home Page: will have store preference, notebook to write down items (POST), backend API from grocery stores. Organize via category...  
         </div>
     )
 }
